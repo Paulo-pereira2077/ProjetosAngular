43f04f946168dac0b254811e44ae4200d24e9f31
Paulo César Rocha <paulo.crrpereira@gmail.com>
2025-10-17 16:30:37 -0300

    adicionando um footer

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,11 +2,12 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './shared/header/header';
 import { FormsModule } from '@angular/forms';
+import { Footer } from "./shared/footer/footer";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, Header, FormsModule],
+  imports: [RouterOutlet, Header, FormsModule, Footer],
   templateUrl: './app.html',  
   styleUrls: ['./app.css']
 })
