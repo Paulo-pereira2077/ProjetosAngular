5b6ac8f3089f8402947a41af3979786df87d6b73
Paulo César Rocha <paulo.crrpereira@gmail.com>
2025-10-17 16:19:07 -0300

    adicionando os campos para o usuário digitar nome, sobrenome e cidade

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,28 +1,30 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './shared/header/header';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, Header],
+  imports: [RouterOutlet, Header, FormsModule],
   templateUrl: './app.html',  
   styleUrls: ['./app.css']
 })
 export class App {
- titulo = 'Olá Paulo';
- nome = 'Bem-vindos ao angular!';
+  titulo = 'Alunos'
+ nome = ''
+ sobrenome = '';
+ cidade = '';
  textoBotao: string = 'Clique aqui!';
- botaoDesabilitado: boolean = false;
- keypress: boolean = false;
- mensagem: string = '';
- 
- onbotaoClicado() {
-  this.mensagem = 'Você clicou no botão!'
- }
-
- onKeyPressed() {
-  this.mensagem = 'Digitando Texto'
- }
+ botaoDesabilitado: boolean = true;
+ mensagem: string = ''; 
+ onBotaoClicado() {
+  console.log(this.nome)  
+  alert(`Olá ${this.nome} ${this.sobrenome} !`) ;
+  }
+ onKeyUp(event: Event) {
+    const input = event.target as HTMLInputElement;
+    this.mensagem = `Olá ${input.value} !`;
+  } 
  
 }
