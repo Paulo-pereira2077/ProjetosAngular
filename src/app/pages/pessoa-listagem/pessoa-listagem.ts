92036cc96751b760a52c0f11485c96f25037a811
Paulo <pcrrp.pereira@gmail.com>
2025-11-07 17:04:39 -0300

    ajustando algumas coisas, porém ainda está com erro no routerLink e no ngModel

--- src/app/pages/pessoa-listagem/pessoa-listagem.ts
+++ src/app/pages/pessoa-listagem/pessoa-listagem.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+ import { Component, OnInit } from '@angular/core';
+ import { PessoaService } from '../../core/services/pessoa.service';
+ import { Pessoa } from '../../core/services/types/types';
+ import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-pessoa-listagem',
@@ -7,6 +10,23 @@ import { Component } from '@angular/core';
   templateUrl: './pessoa-listagem.html',
   styleUrl: './pessoa-listagem.css'
 })
-export class PessoaListagem {
+export class PessoaListagemComponent implements OnInit {
+ listaPessoas: Pessoa[] = [];
+ constructor(
+ private service: PessoaService,
+ private router: Router
+ ){}
+ ngOnInit(): void {
+ this.service.listar().subscribe((pessoas)=>{
+ this.listaPessoas = pessoas;
+ });
+ }
+ excluir(id:number){
+ if(id){
+ this.service.excluir(id).subscribe(() => {
+ window.location.reload()
+ })
+ }
+ }
 
 }
